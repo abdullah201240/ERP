f30fd2ac9f6844a48da6541ce8741467d0b2ec8c
abdullah201240 <abdullahalsakib7075@gmail.com>
2025-02-06 11:04:39 +0600

    3

--- admin/server/src/routes/index.ts
+++ admin/server/src/routes/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import employeeRoute from "./employeeRoute";
-import productRoute from "./product";
 
 
 
@@ -12,7 +11,6 @@ const router = express.Router();
  * Mount example routes under /api/v1/example
  */
 router.use("/employee", employeeRoute);
-router.use("/product", productRoute);
 
 
 
